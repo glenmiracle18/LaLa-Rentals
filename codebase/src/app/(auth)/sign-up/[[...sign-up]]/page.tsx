fcce99e847e69b5022990ecffc5e9a30f73372d0
glenmiracle18 <m.bonyu@alustudent.com>
2025-02-21 12:10:14 +0200

    fixed config

--- codebase/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ codebase/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { SignUp } from "@clerk/nextjs";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../../../components/ui/card";
 import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
--- codebase/tailwind.config.ts
+++ codebase/tailwind.config.ts
@@ -7,7 +7,8 @@ export default withUt({
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	"./src/**/*.{js,ts,jsx,tsx,mdx}"
+	"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/app/(auth)/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
   	extend: {
